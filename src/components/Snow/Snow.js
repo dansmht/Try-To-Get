4f211dc71e0cd8d6489d4b7b1e5d38eb995818ee
advden4ik <dan12gm@mail.ru>
2020-01-14 18:19:31 +0300

    Fixed component Snow

--- src/components/Snow/Snow.js
+++ src/components/Snow/Snow.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Snow.css'
 
-const Snow = () => Array(...Array(200)).map((_, i) => <div key={i} className='snow' />)
+const Snow = () => {
+    return <div>
+        {Array(...Array(200)).map((_, i) => <div key={i} className='snow' />)}
+    </div>
+}
 
 export default Snow
