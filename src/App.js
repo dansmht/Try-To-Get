758cb2d9e1d709a6e2c86ed8f4780ab05395fbfe
advden4ik <dan12gm@mail.ru>
2020-02-06 15:23:45 +0300

    Added tab Rules

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import { Route, Switch } from 'react-router-dom'
 import Game from './components/Game/Game'
+import Rules from './components/Rules/Rules'
 import Menu from './components/Menu/Menu'
 import BestScoresContainer from './components/BestScores/BestScoresContainer'
 import Soon from './components/Soon/Soon'
@@ -12,8 +13,9 @@ const App = () => {
       <div className="container">
         <Switch>
           <Route exact path='/' component={Menu} />
-          <Route path='/game' component={Game} />
-          <Route path='/best' component={BestScoresContainer} />
+          <Route exact path='/game' component={Game} />
+          <Route exact path='/rules' component={Rules} />
+          <Route exact path='/best' component={BestScoresContainer} />
           <Route component={Soon} />
         </Switch>
       </div>
